773c7d257814da6d60c9605e8978373d1f431062
Chiranjeevi Dasatti <chiranjeevi.dasatti@planonsoftware.com>
2020-11-14 02:08:42 +0530

    Addef first 3 tests and ci configuration

---
+++ applitools.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  concurrency: 1,
+  apiKey: 'asBHJW103104YdHWK0KqJysLAuWEwNil110u9798997X1029m3ebpc110',
+  browserCombo: [
+    // Add browsers with different viewports
+    { width: 1200, height: 800, name: 'chrome' },
+  ],
+  // set batch name to the configuration
+  batchName: 'Testing Lifecycle',
+};
---
+++ cypress/integration/appli_fashion/AppliFashionTest.spec.js
@@ -0,0 +1,47 @@
+var config = require('../../../applitools.config');
+const appName = 'AppliFashion';
+
+var url = 'https://demo.applitools.com/tlcHackathonMasterV1.html';
+
+function eyesCheck(params) {
+  Cypress.config('eyesTimeout');
+  cy.eyesOpen({
+    appName: appName,
+    batchName: config.batchName,
+    browser: config.browserCombo,
+    testName: params.testName,
+    stepName: params.stepName,
+  });
+
+  if (params.target != 'region') {
+    cy.eyesCheckWindow(params.testName);
+  } else {
+    cy.eyesCheckWindow({
+      tag: params.testName,
+      target: 'region',
+      selector: params.selector,
+      fully: true,
+    });
+  }
+
+  cy.eyesClose();
+}
+
+describe('AppliFashionTests', () => {
+  it(`Test 1`, function() {
+    cy.visit(url);
+    eyesCheck({ testName: this.test.title, stepName: 'mainPage' });
+  });
+
+  it(`Test 2`, function() {
+    eyesCheck({ testName: this.test.title, stepName: 'filter by color', target: 'region', selector: '#product_grid' });
+  });
+
+  it(`Test 3`, function() {
+    cy.get('#IMG__imgfluid__215').click();
+    cy.get('.product_details_img').then(($productDetail) => {
+      expect(Cypress.dom.isVisible($productDetail)).to.be.true;
+      eyesCheck({ testName: this.test.title, stepName: 'product details' });
+    });
+  });
+});
