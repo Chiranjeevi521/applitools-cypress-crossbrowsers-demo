acf7a4caf9862ae998aebe1adbe95df146119182
Chiranjeevi521 <chiranjeevi521@gmail.com>
2021-05-29 14:03:50 +0530

    Update applitools.config.js

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   concurrency: 1,
-  apiKey: 'asBHJW103104YdHWK0KqJysLAuWEwNil110u9798997X1029m3ebpc110',
+  apiKey: 'your applitools api key',
   browserCombo: [
     // Add browsers with different viewports
     { width: 1200, height: 800, name: 'chrome' },
